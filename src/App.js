4ceb9af3a30b7078cb3bca25a8215129912afb56
RT Sullivan <atleast24.7@list.ru>
2017-09-04 11:04:10 +0800

    Import new Component into App.js and rendering toggle method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 
+import Dropdown from './Dropdown';
+
 class App extends Component {
- render() {
-   return <div>Hello world</div>
- }
+  constructor(props){
+    super(props);
+    this.state = { isOpened: false };
+  }
+
+  toggleState(){
+    this.setState({ isOpened: !this.state.isOpened })
+  }
+
+  render() {
+    console.log('IsOpened',this.state.isOpened);
+    return (
+     <div onClick={this.toggleState.bind(this)}>
+       <Dropdown />
+     </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Dropdown.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Dropdown extends Component{
+  render(){
+    return <div>It's a dropdown body</div>
+  }
+}
+
+export default Dropdown;
